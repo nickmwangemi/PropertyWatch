888e06461db38d8ae46bf781407599937fd6e5bd
Nick Mwangemi <nickmwangemi@gmail.com>
2020-04-12 16:28:48 +0300

    Added pages app and templates directory

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+
+
+# Create your views here.
+def index(request):
+    return render(request,'pages/index.html')
+
+def about(request):
+    return render(request,'pages/about.html')

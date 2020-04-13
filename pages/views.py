c576a6d39e2c030140b92d5ebabb7c0b0558f061
Nick Mwangemi <nickmwangemi@gmail.com>
2020-04-13 03:38:52 +0300

    Added template tags to render about page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render,redirect
+from listings.models import Listing
+from agents.models import Agent
 
 
 # Create your views here.
 def index(request):
-    return render(request,'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings':listings,
+    }
+    return render(request,'pages/index.html',context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    # Get all agents
+    agents = Agent.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_agents = Agent.objects.all().filter(is_mvp=True)
+
+    context = {
+        'agents':agents,
+        'mvp_agents':mvp_agents,
+    }
+    
+    return render(request,'pages/about.html',context)

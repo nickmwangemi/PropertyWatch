6d4c21e8cbd3e2b83470b3379f73eb1e1783b0c7
Nick Mwangemi <nickmwangemi@gmail.com>
2020-04-17 02:16:59 +0300

    Renamed project from ProperWatch to PropertyWatch

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ProperWatch
+# PropertyWatch
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProperWatch.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PropertyWatch.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
